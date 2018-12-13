a28c745801092655aab040623e9167c250e90f38
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2018-12-13 20:43:35 +0100

    show hexdump

--- usbitem.cpp
+++ usbitem.cpp
@@ -68,7 +68,7 @@ QVariant USBItem::data(int column) const
             if(m_record->getPid() == PID_SOF) {
                 return QString("Frame: %1").arg(m_record->m_FrameNumber);
             }
-            return m_record->m_Data.toHex();
+            return m_record->m_Data.toHex(' ');
         default:
             return QVariant();
     }
@@ -84,11 +84,53 @@ USBItem *USBItem::parentItem()
     return m_parentItem;
 }
 
+QString formatHexdump(const QByteArray &data) {
+    QString out;
+
+    char ascii[17];
+    int i, j;
+    int addr = 0;
+
+    ascii[16] = '\0';
+    for (i = 0; i < data.size(); ++i) {
+        if ((i % 16) == 0) {
+            out += QString("%1  ").arg(addr, 4, 16, QChar('0'));
+            addr += 16;
+        }
+
+        out += QString("%1 ").arg(static_cast<unsigned char>(data[i]), 2, 16, QChar('0'));
+        if (data[i] >= ' ' && data[i] <= '~') {
+            ascii[i % 16] = data[i];
+        } else {
+            ascii[i % 16] = '.';
+        }
+        if ((i+1) % 8 == 0 || i+1 == data.size()) {
+            out += " ";
+            if ((i+1) % 16 == 0) {
+                out += "|";
+                out += ascii;
+                out += "|\n";
+            } else if (i+1 == data.size()) {
+                ascii[(i+1) % 16] = '\0';
+                if ((i+1) % 16 <= 8) {
+                    out += " ";
+                }
+                for (j = (i+1) % 16; j < 16; ++j) {
+                    out += "   ";
+                }
+                out += "|";
+                out += ascii;
+                out += "|\n";
+            }
+        }
+    }
+
+    return out;
+}
+
 const QString USBItem::asciiData()
 {
-    return m_record->m_Data.toHex();
-
-    // m_packet->m_Data.data(), m_packet->m_DataLen
+    return formatHexdump(m_record->m_Packet);
 }
 
 const QString USBItem::details()
